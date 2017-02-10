5950d62046d828356b7bc0aa352c9d186b245e10
Chris McKnight <cmckni3@gmail.com>
2017-02-09 21:13:48 -0600

    feat(badge): Add complete course badge elements

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
   // your code will go here
+  $.ajax('https://www.codeschool.com/users/cmckni3.json', {
+    dataType: 'jsonp',
+    success: function(response) {
+      var completed_courses = response.courses.completed;
+      var course_badges = completed_courses.map(function(course) {
+        var badge = document.createElement('div');
+        badge.setAttribute('class', 'course');
+        return badge;
+      }).forEach(function(badge) {
+        badges.appendChild(badge);
+      });
+    },
+    error: function(xhr, textStatus, error) {
+      console.log(error);
+    }
+  });
 
 });
