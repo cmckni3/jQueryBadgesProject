9b921f15e85eabc1d10a3d00d8bd6b67ec103cf9
Chris McKnight <cmckni3@gmail.com>
2017-02-09 21:18:03 -0600

    feat(badge): Add title and image

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,14 @@ $(function() {
       var course_badges = completed_courses.map(function(course) {
         var badge = document.createElement('div');
         badge.setAttribute('class', 'course');
+
+        var title = document.createElement('h3');
+        title.appendChild(document.createTextNode(course.title));
+        badge.appendChild(title);
+
+        var image = document.createElement('img');
+        image.setAttribute('src', course.badge);
+        badge.appendChild(image);
         return badge;
       }).forEach(function(badge) {
         badges.appendChild(badge);
